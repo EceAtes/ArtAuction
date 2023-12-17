326531d03f1e16cffb6d7b7db4c8a5f76026b916
zbegum <begum.kara@ug.bilkent.edu.tr>
2023-12-18 00:57:20 +0300

    exhibition bug fixed

--- src/main/java/com/example/artauction/repository/ExhibitionRepository.java
+++ src/main/java/com/example/artauction/repository/ExhibitionRepository.java
@@ -23,9 +23,11 @@ public class ExhibitionRepository {
 
     
     public List<ExhibitionDTO> listAllExhibitions() {
-        String sql = "SELECT e.exhibitionID, e.exhibitionName, e.exhibitionDescriptor, c.auctionID " +
+        String sql = "SELECT e.exhibitionID, e.exhibitionName, e.exhibitionDescriptor, " +
+                     "GROUP_CONCAT(c.auctionID) AS auctionIDs " +
                      "FROM exhibition e " +
-                     "LEFT JOIN curate c ON e.exhibitionID = c.exhibitionID";
+                     "LEFT JOIN curate c ON e.exhibitionID = c.exhibitionID " +
+                     "GROUP BY e.exhibitionID";
     
         List<ExhibitionDTO> exhibitionDTOList = jdbcTemplate.query(sql, (resultSet, i) -> {
             ExhibitionDTO exhibitionDTO = new ExhibitionDTO();
@@ -33,17 +35,20 @@ public class ExhibitionRepository {
             exhibitionDTO.setExhibitionName(resultSet.getString("exhibitionName"));
             exhibitionDTO.setExhibitionDescriptor(resultSet.getString("exhibitionDescriptor"));
     
-            int auctionId = resultSet.getInt("auctionID");
-            if (auctionId > 0) {
-                String sqlAuction = "SELECT * FROM `Auction` WHERE `auctionID` = ?";
-                RowMapper<AuctionDTO> rowMapper = (rs, rowNum) -> {
-                    AuctionDTO auction = new AuctionDTO();
-                    auction.setAuctionID(auctionId);
-                    auction.setTitle(rs.getString("title"));
-                    return auction;
-                };
-                AuctionDTO auction = jdbcTemplate.queryForObject(sqlAuction, rowMapper, auctionId);
-                exhibitionDTO.getAuctions().add(auction);
+            String auctionIDs = resultSet.getString("auctionIDs");
+            if (auctionIDs != null) {
+                String[] auctionIDArray = auctionIDs.split(",");
+                for (String auctionID : auctionIDArray) {
+                    String sqlAuction = "SELECT * FROM `Auction` WHERE `auctionID` = ?";
+                    RowMapper<AuctionDTO> rowMapper = (rs, rowNum) -> {
+                        AuctionDTO auction = new AuctionDTO();
+                        auction.setAuctionID(Integer.parseInt(auctionID));
+                        auction.setTitle(rs.getString("title"));
+                        return auction;
+                    };
+                    AuctionDTO auction = jdbcTemplate.queryForObject(sqlAuction, rowMapper, Integer.parseInt(auctionID));
+                    exhibitionDTO.getAuctions().add(auction);
+                }
             }
             return exhibitionDTO;
         });
